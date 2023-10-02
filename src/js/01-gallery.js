56cc9a68f95b6f8bda0ab1b3c49e3ce277366404
StivenAguirre <105611680+StivenAguirre@users.noreply.github.com>
2023-10-01 21:31:36 -0500

    commit algunos cambios

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,49 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+
+import { galleryItems } from './gallery-items.js';
+
+import simpleLightbox from 'simplelightbox';
+
+const galleryImages = (item) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`
+   
+}
+
+
+
+
+
+const itemContainer = document.querySelector(".gallery");
+for (const item of galleryItems) {
+    const newItem = galleryImages(item);
+    itemContainer.innerHTML += newItem;
+
+ };
+ itemContainer.addEventListener("click", (event) => {
+  event.preventDefault();
+      const instance = basicLightbox.create(`
+          <img src="${event.target.dataset.source}" alt="${event.target.alt}" />
+      `);
+      
+      instance.show();
+
+      const closeEsc = (event) => {
+          if (event.key === "Escape") {
+            instance.close();
+            document.removeEventListener("keydown", closeEsc);
+          }
+      };
+      document.addEventListener("keydown", closeEsc);
+  }
+);
